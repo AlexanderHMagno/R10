3cb52d1efe77dffb91eb2260d28fdcc64e9aff1c
Alex - Home <alexander.hortua10@gmail.com>
2019-07-28 15:35:30 -0700

    adding the map with the indicator position

--- ios/R10/AppDelegate.m
+++ ios/R10/AppDelegate.m
@@ -10,11 +10,15 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
+
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyDhPqSCxlELpVsNzpLMm_-XyOIUPbaB3Sc"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"R10"
--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,20 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View, Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const Map = ({ data }) => {
-  return (
-    <View>
-      {data.map(({ id, title, description }) => (
-        <View key={id}>
-          <Text>{title}</Text>
-        </View>
-      ))}
-    </View>
-  );
-};
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: "",
+      markers: []
+    };
+  }
+
+  componentWillMount() {
+    const point = {
+      latitude: 49.263455,
+      longitude: -123.138064,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    };
+    this.setState({
+      region: point,
+      markers: [{ title: "R10", coordinates: point }]
+    });
+    // console.log(this.state.markers);
+    // console.log(typeof this.state.markers);
+  }
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  render() {
+    return (
+      <MapView
+        region={this.state.region}
+        // onRegionChange={this.onRegionChange.bind(this)}
+        style={{ width: "100%", height: "100%" }}
+        showsTraffic="false"
+        loadingEnabled="true"
+      >
+        {this.state.markers.map(marker => (
+          <Marker
+            coordinate={marker.coordinates}
+            title={marker.title}
+            image={require("../../assets/images/map_pin.png")}
+            // description={marker.description}
+          />
+        ))}
+      </MapView>
+    );
+  }
+}
 
 Map.propTypes = {
-  data: PropTypes.array.isRequired
+  data: PropTypes.array
 };
 export default Map;
